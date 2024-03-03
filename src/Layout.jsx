1ec2acca7a8405959f886872f3c01fe2ab731332
Burak <burak.bilici@mobilistbilisim.com>
2024-03-03 16:21:41 +0300

    updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
-import { Header } from "./components/header/Header"
-import Footer from "./components/footer/Footer"
+import { Header } from "./components/main/header/Header"
+import Footer from "./components/main/footer/Footer"
 
  const Layout = ({children}) => {
   return (
